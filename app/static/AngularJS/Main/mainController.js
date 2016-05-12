470a30c46e5832db3d05085682d4cfeff7275c62
urielgangster <porkpie_mod@hotmail.com>
2016-05-11 19:18:34 -0500

    Chat
    Se mejora el chat

--- app/static/AngularJS/Main/mainController.js
+++ app/static/AngularJS/Main/mainController.js
@@ -1,22 +1,33 @@
-registrationModule.controller('mainController', function ($scope, $rootScope, $location, mainRepository) {
+registrationModule.controller('mainController', function ($scope, $rootScope, $location, localStorageService, mainRepository) {
     $scope.showChat = $location.path();
-    
+    $scope.idUsuario = 3;
+    var idCita = localStorageService.get('cita');
+    $scope.comentarios = '';
+
     $scope.init = function () {
-        
+
         $scope.cargaChat();
     }
 
     $scope.cargaChat = function () {
-            mainRepository.getChat(5).then(function (result) {
+        mainRepository.getChat(idCita).then(function (result) {
+            if (result.data.length > 0) {
                 $scope.chat = result.data;
-            }, function (error) {});
+            }
+        }, function (error) {});
     }
 
     $scope.EnviarComentario = function (comentarios) {
         mainRepository.putMessage(3, comentarios, idCita).then(function (result) {
                 $scope.algo = result.data;
+                $scope.clearComments();
                 $scope.cargaChat();
+
             },
             function (error) {});
+    }
+
+    $scope.clearComments = function () {
+        $scope.comentarios = '';
     }
 });
